3f4eb11f85128455bbdd54f3892095092f88f025
Chin <>
2025-08-09 10:34:17 +0800

    CND解析

--- components/CustomImage.tsx
+++ components/CustomImage.tsx
@@ -1,22 +1,10 @@
 // components/Image.tsx
 import Image from "next/image";
 
-
-const rawBase = process.env.NEXT_PUBLIC_BASE_PATH || "";
-// 确保前置斜杠、且去掉多余的尾斜杠
-const basePath = rawBase ? `/${rawBase.replace(/^\/|\/$/g, "")}` : "";
-
 export function CusImage(props) {
   const { src, ...rest } = props;
-  let finalSrc = src;
 
-  if (typeof src === 'string' && src.startsWith('/')) {
-    // 去掉初始斜杠后与 basePath 拼接
-    const trimmed = src.slice(1);
-    finalSrc = `${basePath}/${trimmed}`;
-  }
-
-  return <Image src={finalSrc} {...rest} />;
+  return <Image src={src} {...rest} />;
 }
 
 export default CusImage;
